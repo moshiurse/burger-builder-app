a5017adc434d5a362035a6d6ef07f127cfe1bab6
Moshiur Rahman <rmoshiur705@gmail.com>
2020-02-01 20:57:13 +0600

    dynamically added burger ingredients to burger builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,13 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 import Aux from "../../hoc/Aux";
 
+const INGREDIENT_PRICE = {
+  salad: 0.5,
+  meat: 1.5,
+  bacon: 0.9,
+  cheese: 1.2
+};
+
 class BurgerBuilder extends Component {
   constructor(props) {
     super(props);
@@ -15,15 +22,33 @@ class BurgerBuilder extends Component {
         meat: 0,
         bacon: 0,
         cheese: 0
-      }
+      },
+      totalPrice: 4
     };
   }
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredient = {
+      ...this.state.ingredients
+    };
+    updatedIngredient[type] = updatedCount;
+
+    const priceAddition = INGREDIENT_PRICE[type];
+    const oldPrice = this.state.totalPrice;
+    const updatedPrice = oldPrice + priceAddition;
+
+    this.setState({ totalPrice: updatedPrice, ingredients: updatedIngredient });
+  };
+
+  removeIngredientHandler = type => {};
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
