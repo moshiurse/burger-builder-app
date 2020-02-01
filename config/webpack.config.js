fb810ebce1ff46ba7e92fe5f9ccc4ad7fc5f87b1
Moshiur Rahman <rmoshiur705@gmail.com>
2020-02-01 15:58:44 +0600

    Added Burger Ingredients To Burger builder

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -88,7 +88,14 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve("css-loader"),
-        options: cssOptions
+        options: {
+          // cssOptions,
+          importLoaders: 1,
+          // modules: true,
+          modules: {
+            localIdentName: "[name]__[local]___[hash:base64:5]"
+          }
+        }
       },
       {
         // Options for PostCSS as we reference these options twice
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
+import Burger from "../../components/Burger/Burger";
+
 import Aux from "../../hoc/Aux";
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>burger</div>
+        <Burger />
         <div>builder</div>
       </Aux>
     );
