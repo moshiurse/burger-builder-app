a591f4af723d35519900e91eae707c715aa37990
Moshiur Rahman <rmoshiur705@gmail.com>
2020-02-12 08:03:24 +0600

    improve performance by using shouldComponentUpdate

--- src/components/Burger/OrderSummery/OrderSummery.js
+++ src/components/Burger/OrderSummery/OrderSummery.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const OrderSummery = props => {
-  const ingredientSumery = Object.keys(props.ingredients).map(igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "uppercase" }}>{igKey}</span> :
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
+class OrderSummery extends Component {
+  render() {
+    const ingredientSumery = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "uppercase" }}>{igKey}</span> :
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
 
-  return (
-    <Aux>
-      <h3>Order Summery</h3>
-      <p>The burger ingredients are: </p>
-      <ul>{ingredientSumery}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>Order Summery</h3>
+        <p>The burger ingredients are: </p>
+        <ul>{ingredientSumery}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
 export default OrderSummery;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Aux";
 import BackDrop from "../BackDrop/BackDrop";
 
 import classes from "./Modal.css";
-const Modal = props => {
-  return (
-    <Aux>
-      <BackDrop show={props.show} clicked={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log("MODALWILLUPDTE");
+  }
+
+  render() {
+    return (
+      <Aux>
+        <BackDrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 export default Modal;
